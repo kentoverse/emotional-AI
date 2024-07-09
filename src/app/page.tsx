e2f2e6227b515439798b4f114b96fdb6b4b20544
kentoverse <kentoverse@gmail.com>
2024-07-09 14:22:29 -0400

    wwwwww

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Home from '@/src/views/home/Home';
+import { HomePageAttributes } from '@/src/types';
 
 export default function Page() {
-  const pageData = {
+  const pageData: HomePageAttributes = {
     greeting: 'Hi Authentic Human,',
     title: 'Welcome to Emotional AI',
-    subtitle:
-      'The Emotional AI project aims to create a comprehensive platform for introspection and emotional analysis. The project is structured to serve multiple purposes, including acting as an API server, providing an admin UI for data management and analysis, and planning for future expansion to include a cross-platform mobile version for public users. Using IBM InspectorRAGet as the base for building our AI data model offers significant advantages, including a proven framework, advanced metrics, scalability, integration capabilities, and strong community support. These benefits make it an ideal foundation for developing a sophisticated and effective AI solution.',
+    subtitle: 'The Emotional AI project aims to create a comprehensive platform for introspection and emotional analysis. The project is structured to serve multiple purposes, including acting as an API server, providing an admin UI for data management and analysis, and planning for future expansion to include a cross-platform mobile version for public users. Using IBM InspectorRAGet as the base for building our AI data model offers significant advantages, including a proven framework, advanced metrics, scalability, integration capabilities, and strong community support. These benefits make it an ideal foundation for developing a sophisticated and effective AI solution.',
     subtitleLink: {
       content: 'Learn More',
       href: 'https://github.com/kentoverse/emotional-AI',
@@ -18,7 +18,7 @@ export default function Page() {
         href: '/visualize',
         actionText: 'Try it out',
         tag: null,
-        icon: 'CHART_MULTITYPE', // Must match one of the specific string literals
+        icon: 'CHART_MULTITYPE', // Ensure this matches the specific string literal
         openInNewTab: false,
       },
       {
@@ -27,7 +27,7 @@ export default function Page() {
         href: '/examples',
         actionText: 'Explore',
         tag: null,
-        icon: 'MICROSCOPE', // Must match one of the specific string literals
+        icon: 'MICROSCOPE', // Ensure this matches the specific string literal
         openInNewTab: false,
       },
       {
@@ -36,7 +36,7 @@ export default function Page() {
         href: '/cookbooks',
         actionText: 'Start cooking',
         tag: 'coming soon',
-        icon: 'NOODLE_BOWL', // Must match one of the specific string literals
+        icon: 'NOODLE_BOWL', // Ensure this matches the specific string literal
         openInNewTab: false,
         disabled: true,
       },
@@ -46,7 +46,7 @@ export default function Page() {
         href: '/documentation',
         actionText: 'Read the docs',
         tag: 'coming soon',
-        icon: 'BOOK', // Must match one of the specific string literals
+        icon: 'BOOK', // Ensure this matches the specific string literal
         openInNewTab: false,
         disabled: true,
       },
